5e0192d34262649d0c540981585ec6985cf66314
Mondo <mondogao@gmail.com>
2017-06-03 16:44:58 +0800

    feat: 完成划线

--- src/components/App.js
+++ src/components/App.js
@@ -19,8 +19,8 @@ class App extends React.Component {
   threeInit = () => {
     let scene = new three.Scene()
     let camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 1000)
+    
     let renderer = new three.WebGLRenderer()
-  
     renderer.setSize(window.innerWidth, window.innerHeight)
     this.wrapper.appendChild(renderer.domElement)
     
@@ -31,16 +31,11 @@ class App extends React.Component {
     })
   }
   
-  threeAnimate = () => {
-    let geometry = new three.BoxGeometry(1,1,1)
-    let material = new three.MeshBasicMaterial({
-      color: 0x00ff00
-    })
-    let cube = new three.Mesh(geometry, material)
+  threeRender = () => {
+    requestAnimationFrame(this.threeRender)
+    this.state.renderer.render(this.state.scene, this.state.camera)
     
-    this.state.camera.position.z = 5
-    
-    this.threeAddItem(cube, 'cube')
+    // this.threeRotateCube()
   }
   
   threeAddItem = (item, name) => {
@@ -53,22 +48,42 @@ class App extends React.Component {
     }
   }
   
+  threeCreateEle = () => {
+    // let geometry = new three.BoxGeometry(1,1,1)
+    // let material = new three.MeshBasicMaterial({
+    //   color: 0x00ff00
+    // })
+    // let cube = new three.Mesh(geometry, material)
+    //
+    // this.state.camera.position.z = 5
+    //
+    // this.threeAddItem(cube, 'cube')
+    this.state.camera.position.set(0, 0, 100)
+    this.state.camera.lookAt(new three.Vector3(0, 0, 0))
+    
+    let material = new three.LineBasicMaterial({
+      color: 0x0000ff
+    })
+    let geometry = new three.Geometry()
+    
+    geometry.vertices.push(new three.Vector3(-10, 0, 0))
+    geometry.vertices.push(new three.Vector3(0, 10, 0))
+    geometry.vertices.push(new three.Vector3(10, 0, 0))
+    
+    let line = new three.Line(geometry, material)
+    
+    this.threeAddItem(line, 'line')
+  }
+  
   threeRotateCube = () => {
     if (this.items && this.items.cube) {
       this.items.cube.rotation.x += .1
     }
   }
   
-  threeRender = () => {
-    requestAnimationFrame(this.threeRender)
-    this.state.renderer.render(this.state.scene, this.state.camera)
-    
-    this.threeRotateCube()
-  }
-  
   componentDidUpdate(prevProps, prevState) {
     if (!prevState.scene && this.state.scene) {
-      this.threeAnimate()
+      this.threeCreateEle()
       this.threeRender()
     }
   }
