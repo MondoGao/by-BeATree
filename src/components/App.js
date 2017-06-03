08545ab9b9def204b2afe8d35dddf2329db209fe
Mondo <mondogao@gmail.com>
2017-06-03 15:53:21 +0800

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import {
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom'
+
+import styles from './App.scss'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Be a Tree
+      </div>
+    )
+  }
+}
+
+export default App
