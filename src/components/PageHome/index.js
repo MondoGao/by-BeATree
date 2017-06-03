68a12462205ccf22db5379e898614c16899b7de2
Mondo <mondogao@gmail.com>
2017-06-04 01:09:35 +0800

    feat: 完成主页效果

--- src/components/PageHome/index.js
+++ src/components/PageHome/index.js
@@ -1,12 +1,38 @@
-import React from 'React'
+import React from 'react'
 
 import styles from './PageHome.scss'
+import imgTree from 'assets/scene-1-tree@2x.png'
 
 class PageHome extends React.Component {
+  state = {
+    isEntering: false
+  }
+  
+  handleStartClick = () => {
+    this.setState({
+      isEntering: true
+    })
+    
+    setTimeout(() => {
+      this.props.history.push('/scene')
+    }, 1000)
+  }
+  
   render() {
     return (
-      <div>
-        HomePage
+      <div className={`${styles.page}  ${this.state.isEntering ? styles.entering : ''}`}>
+        <div className={styles.wrapper}>
+          <div>
+            <img
+              src={imgTree}
+              alt="Tree"
+              className={styles.tree}/>
+          </div>
+          <span
+            className={styles['btn-start']}
+            onClick={this.handleStartClick}>be a tree</span>
+        </div>
+        <div className={styles['dark-side']}/>
       </div>
     )
   }
